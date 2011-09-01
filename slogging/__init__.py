4416a364da5354c4c54849ca047a7cfd7c437107
John Dickinson <me@not.mn>
2011-09-01 11:30:20 -0500

    bumped version for continued dev work

--- slogging/__init__.py
+++ slogging/__init__.py
@@ -1,7 +1,7 @@
 import gettext
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 1, 3)
+version_info = (1, 1, 3, 'dev')
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 
