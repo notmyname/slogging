9823ad4365080f42b1b35c119e94abef48f6c650
John Dickinson <me@not.mn>
2012-02-28 09:43:24 -0600

    version number bump

--- slogging/__init__.py
+++ slogging/__init__.py
@@ -1,7 +1,7 @@
 import gettext
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 1, 4)
+version_info = (1, 1, 5)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 
