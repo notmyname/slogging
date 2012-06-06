b1286ddc4d5e0c2096ab5797e3d7cfc9fd64d0dd
John Dickinson <me@not.mn>
2012-06-06 21:07:39 +0000

    releasing 1.1.8

--- slogging/__init__.py
+++ slogging/__init__.py
@@ -1,7 +1,7 @@
 import gettext
 
 #: Version information (major, minor, revision[, 'dev']).
-version_info = (1, 1, 7)
+version_info = (1, 1, 8)
 #: Version string 'major.minor.revision'.
 version = __version__ = ".".join(map(str, version_info))
 
